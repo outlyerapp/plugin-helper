f91444b6c09c29bf3969302d5567ea59f26abf3e
Colin Hemmings <colin.hemmings@gmail.com>
2017-03-31 17:58:12 +0100

    fix test for subprocess

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,6 +1,4 @@
-import subprocess
 import unittest
-from mock import Mock
 from test.test_support import EnvironmentVarGuard
 from outlyer.plugin_helper import container
 
@@ -28,28 +26,4 @@ class TestDockerHelpers(unittest.TestCase):
     def test_conatiner_ip(self):
         self.env.set('CONTAINER_IP', CONTAINER_IP)
         self.assertEqual(CONTAINER_IP, container.get_container_ip())
-
-    def test_patch_container(self):
-        container.patch()
-        self.assertTrue(hasattr(subprocess, '_check_output'))
-
-    def test_unpatch_container(self):
-        container.unpatch()
-        self.assertFalse(hasattr(subprocess, '_check_output'))
-
-    def test_check_output_no_container_patched(self):
-        self.env.unset('CONTAINER_ID')
-        container.patch()
-        subprocess._check_output = Mock()
-        subprocess.check_output = Mock()
-        container.check_output(['ls', '/tmp'])
-        subprocess._check_output.assert_called_with(['ls', '/tmp'])
-        subprocess.check_output.assert_not_called()
-
-    def test_check_output_no_container_not_patched(self):
-        self.env.unset('CONTAINER_ID')
-        container.unpatch()
-        subprocess.check_output = Mock()
-        container.check_output(['ls', '/tmp'])
-        subprocess.check_output.assert_called_with(['ls', '/tmp'])
 
